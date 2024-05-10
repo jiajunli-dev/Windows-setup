5fc6cee28c2c1a33a43a8717c80cf70dba545937
jiajunli188 <jiajun.li188@gmail.com>
2024-05-10 19:34:24 +0200

    fix

--- scripts/enable-features.ps1
+++ scripts/enable-features.ps1
@@ -7,6 +7,7 @@ Enable-WindowsOptionalFeature -FeatureName VirtualMachinePlatform -All -Online
 # Enable Windows Subsystem for Linux (WSL)
 Enable-WindowsOptionalFeature -FeatureName Microsoft-Windows-Subsystem-Linux -All -Online
 
+Write-Host "Enableing features done. Restarting computer in 3 seconds..."
 Start-Sleep -Seconds 3
 
 # Restart computer
